d94f3316431f7cfefcbd28abd8c2a5238cb1c214
pjsier <pjsier@gmail.com>
2024-04-16 19:51:57 -0400

    feat: add bseed_special

---
+++ test/test_bseed_special.py
@@ -0,0 +1,37 @@
+import pytest
+from freezegun import freeze_time
+from scrapy.http import HtmlResponse
+
+from tracker.items import TrackerLocation
+from tracker.spiders.bseed_special import BseedSpecialSpider
+
+pytest.mark.usefixtures("betamax_session")
+
+
+@pytest.fixture
+def freeze():
+    with freeze_time("2024-04-16"):
+        yield
+
+
+@pytest.fixture
+def parsed(betamax_session, freeze):
+    url = "https://detroitmi.gov/events/special-land-use-hearings-0"
+    betamax_res = betamax_session.get(url)
+    res = HtmlResponse(url, body=betamax_res.content)
+    spider = BseedSpecialSpider()
+    return [item for item in spider.parse(res)]
+
+
+def test_count(parsed):
+    assert len(parsed) == 3
+
+
+def test_id(parsed):
+    assert parsed[0].id == "bseed_special/2024/04/17/SLU2023-00240"
+
+
+def test_locations(parsed):
+    assert parsed[0].locations == [
+        TrackerLocation(address="7214 W. Vernor Hwy..", pin="18001417.")
+    ]
---
+++ tracker/spiders/bseed_special.py
@@ -0,0 +1,82 @@
+import scrapy
+from datetime import datetime, timedelta
+from scrapy.http import Response
+from typing import List, Optional
+from urllib.parse import urlencode
+from tracker.utils import clean_spaces
+from tracker.items import TrackerEvent, TrackerLocation
+import re
+
+
+class BseedSpecialSpider(scrapy.Spider):
+    name = "bseed_special"
+    allowed_domains = ["detroitmi.gov"]
+
+    def start_requests(self):
+        start_date = datetime.now() - timedelta(days=365)
+        req_params = {
+            "field_start_value": start_date.strftime("%Y-%m-%d"),
+            "term_node_tid_depth": "36",
+            "term_node_tid_depth_1": "All",
+            "term_node_tid_depth_2": "All",
+        }
+        yield scrapy.Request(
+            f"https://detroitmi.gov/Calendar-and-Events?{urlencode(req_params)}",
+            self.parse_events,
+        )
+
+    def parse_events(self, response: Response):
+        IGNORE_STRINGS = ["Land Based Projects"]
+        for event in response.css(".view-content .article-title a"):
+            event_title = event.xpath("string()").extract_first()
+            # Assign list of title strings for events that should be ignored
+            if any(s in event_title for s in IGNORE_STRINGS):
+                continue
+            yield scrapy.Request(
+                response.urljoin(event.xpath("@href").extract_first()), self.parse
+            )
+
+        for next_link in response.css("a[title='Go to next page']"):
+            yield scrapy.Request(
+                response.urljoin(next_link.xpath("@href").extract_first()),
+                self.parse_events,
+            )
+
+    def parse(self, response: Response):
+        event_title = " ".join(response.css("h1.title *::text").extract()).strip()
+        event_time = response.css(".event-node time").xpath("@datetime").extract_first()
+        if event_time is None:
+            return
+        event_dt = datetime.strptime(event_time.split("T")[0].strip(), "%Y-%m-%d")
+
+        for row in response.css("article.description p"):
+            row_text = clean_spaces(" ".join(row.css("*::text").extract()))
+            # Only handling clean PIN items
+            if "PIN:" not in row_text:
+                continue
+            row_chunks = row.css("*::text").extract()
+            case_id = self.parse_case_id(row_chunks)
+
+            yield TrackerEvent(
+                id=f"bseed_special/{event_dt.strftime('%Y/%m/%d')}/{case_id}",
+                source="bseed_special",
+                source_title=event_title,
+                date=event_dt.date(),
+                url=response.url,
+                content=row_text,
+                locations=[self.parse_location(row_chunks)],
+            )
+
+    def parse_case_id(self, row_chunks: List[str]) -> Optional[str]:
+        clean_chunks = [chunk.strip() for chunk in row_chunks if chunk.strip()]
+        for idx, chunk in enumerate(clean_chunks):
+            # Get the first element following "Case:"
+            if "Case" in chunk:
+                return clean_chunks[idx + 1]
+        return "x"
+
+    def parse_location(self, row_chunks: List[str]) -> TrackerLocation:
+        location_row = [r for r in row_chunks if "PIN:" in r][0]
+        street_address = location_row.split("(")[0].strip()
+        pin = re.search(r"(?<=\(PIN:).*(?=\))", location_row).group().strip()
+        return TrackerLocation(pin=pin, address=street_address)
